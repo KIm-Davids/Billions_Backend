ee74e02565a0b01954278628db6caf821d5a8a10
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-13 02:46:44 +0100

    Successfuly Deployed

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://https://www.billionsforextrade.vip"},
+		AllowOrigins:     []string{"https://www.billionsforextrade.vip"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
