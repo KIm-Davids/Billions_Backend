92f7c990d2932b3e26dc2ee6c5a717900f99a54c
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-14 15:27:11 +0100

    Added get user info and get withdraw date

--- models/Profit.go
+++ models/Profit.go
@@ -4,7 +4,7 @@ import "time"
 
 type Profit struct {
 	ID        uint    `gorm:"primaryKey"`
-	UserID    uint    `gorm:"not null"`
+	UserID    string  `gorm:"not null"`
 	Amount    float64 `gorm:"not null"`
 	Source    string  `gorm:"not null"` // e.g., "daily profit"
 	CreatedAt time.Time
