d39b4b9c2ca9223ffe0992862fb8d3eed23ddb77
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-15 12:29:52 +0100

    trying to fix the user dashboard

--- controllers/clientController.go
+++ controllers/clientController.go
@@ -219,10 +219,11 @@ func Deposit(c *gin.Context) {
 
 		// Successfully logged the transaction and updated user balance
 		c.JSON(http.StatusOK, gin.H{"message": "Transaction logged", "transaction": tx})
-	} else {
-		// If status is not confirmed, return an appropriate response
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Deposit status not confirmed"})
 	}
+	//else {
+	//	// If status is not confirmed, return an appropriate response
+	//	c.JSON(http.StatusBadRequest, gin.H{"error": "Deposit status not confirmed"})
+	//}
 }
 
 func Withdraw(c *gin.Context) {
