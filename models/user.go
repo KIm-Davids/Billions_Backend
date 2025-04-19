b375031850857fbfd56500c8af139fec384b0a01
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-19 23:20:53 +0100

    Working on the users daily profits

--- models/user.go
+++ models/user.go
@@ -5,12 +5,13 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	//UserID   uint
-	Username   string  `json:"username"`
-	Email      string  `json:"email" gorm:"unique"`
-	Password   string  `json:"password"`
-	ReferID    string  `json:"referrerId" gorm:"unique"`
-	ReferredBy string  `json:"referred_by"` // referrer's ID (not unique)
-	Balance    float64 `json:"balance"`
-	Package    string  `json:"package,omitempty"`
-	Profit     float64 `gorm:"default:0"`
+	Username          string  `json:"username"`
+	Email             string  `json:"email" gorm:"unique"`
+	Password          string  `json:"password"`
+	ReferID           string  `json:"referrerId" gorm:"unique"`
+	ReferredBy        string  `json:"referred_by"` // referrer's ID (not unique)
+	Balance           float64 `json:"balance"`
+	Package           string  `json:"package,omitempty"`
+	Profit            float64 `gorm:"default:0"`
+	ProfitAddedStatus string  `gorm:"not null"`
 }
