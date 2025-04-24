13e2aaf2d2e52b6709b63f030b97f34346d7c802
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-04-24 08:40:35 +0100

    Worked on the referral bonus

--- controllers/clientController.go
+++ controllers/clientController.go
@@ -581,7 +581,7 @@ func FundReferrerBonus(c *gin.Context) {
 	var totalBonus float64
 	if err := initializers.DB.
 		Model(&models.ReferralBonus{}).
-		Where("referrer_id = ? AND transaction_processed = ?", referrer.ReferID, "true").
+		Where("email = ? AND transaction_processed = ?", req.Email, "true").
 		Select("SUM(balance)").
 		Scan(&totalBonus).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch bonus data"})
